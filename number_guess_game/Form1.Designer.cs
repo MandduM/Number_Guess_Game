1e83a71ec160bcd98ca3acc426b386c742bc7b61
Ali Kaan Ekmekci <ekmekcialikaan@gmail.com>
2023-03-25 22:04:00 +0300

    project start

---
+++ number_guess_game/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace number_guess_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtcomputer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtguess = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtcomputer
+            // 
+            this.txtcomputer.Enabled = false;
+            this.txtcomputer.Location = new System.Drawing.Point(263, 119);
+            this.txtcomputer.Name = "txtcomputer";
+            this.txtcomputer.Size = new System.Drawing.Size(190, 22);
+            this.txtcomputer.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(129, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "COMPUTER";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(263, 183);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(177, 46);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "GO";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtguess
+            // 
+            this.txtguess.Location = new System.Drawing.Point(263, 91);
+            this.txtguess.Name = "txtguess";
+            this.txtguess.Size = new System.Drawing.Size(190, 22);
+            this.txtguess.TabIndex = 0;
+            this.txtguess.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtguess_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(141, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "TO GUESS";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(499, 38);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "NUMBER PREDICTION GAME";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 246);
+            this.Controls.Add(this.txtcomputer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtguess);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtcomputer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtguess;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ number_guess_game/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace number_guess_game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            int guess = Convert.ToInt32(txtguess.Text);
+            Random rnd = new Random();
+            int rndNum = rnd.Next(1, 10);
+
+            if (guess > 10)
+            {
+                MessageBox.Show("Do not enter numbers greater than 10.");
+                Clear();
+                txtguess.Focus();
+            }
+            else if (guess < 1)
+            {
+                MessageBox.Show("Do not enter numbers less than 1.");
+                Clear();
+                txtguess.Focus();
+            }
+
+            else if (guess == rndNum)
+            {
+                txtcomputer.Text = rndNum.ToString();
+                MessageBox.Show("RIGHT GUESS");
+                Clear();
+                txtguess.Focus();
+            }
+            else
+            {
+                txtcomputer.Text = rndNum.ToString();
+                MessageBox.Show("WRONG GUESS");
+                //Clear();
+                txtguess.Focus();
+            }
+        }
+
+        private void txtguess_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        void Clear()
+        {
+            txtcomputer.Clear();
+            txtguess.Clear();
+        }
+    }
+}
